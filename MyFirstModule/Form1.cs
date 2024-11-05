32060930f82390730e5c397defe15aaa8d1dbe69
StanB <stanb791@gmail.com>
2024-11-05 19:09:10 +0300

    Добавьте файлы проекта.

---
+++ MyFirstModule/Form1.cs
@@ -0,0 +1,15 @@
+namespace MyFirstModule
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Привет, мир!";
+        }
+    }
+}
